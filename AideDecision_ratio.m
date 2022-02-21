66736cda2e78189b2fc10a5f5d5600e1e9c32706
Meryem <73638032+malamikasr@users.noreply.github.com>
2022-02-21 17:58:29 +0100

    Add files via upload

---
+++ AideDecision_ratio.m
@@ -0,0 +1,14 @@
+E = [6 5 4 5; 5 2 6 7; 4 3 2 5; 3 7 5 4; 1 7 2 9; 2 5 3 3; 5 4 2 9; 3 5 7 4]; %matrice jugements
+P = [1.5 4 3.5 1]; % matrice poids
+
+
+%Ratios par rapport à a
+for i=1:8
+ratio(i)= 1;
+    for j= 1:4
+        ratio(i) = ratio(i)*power((E(i,j)/E(1,j)),P(j)); 
+    end
+end
+
+%Pour calculer le ratio par rapport aux autres actions ai,
+% il suffit de modifier E(1,j) par E(ai,j)
