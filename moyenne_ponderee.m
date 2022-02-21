029b9ba8fa49fa956f1444b450c67dede30f2ad0
Adam Chellaoui <81109051+Adam-Chellaoui@users.noreply.github.com>
2022-02-21 17:37:23 +0100

    Update moyenne_ponderee.m

--- moyenne_ponderee.m
+++ moyenne_ponderee.m
@@ -1,1 +1,3 @@
-function moyenne = moyenne_ponderee(E, P) %E matrice jugements, P matrice poids
+E = [6 5 4 5; 5 2 6 7; 4 3 2 5; 3 7 5 4; 1 7 2 9; 2 5 3 3; 5 4 2 9; 3 5 7 4]; %matrice jugements
+P = [1.5 4 3.5 1]; % matrice des poids
+moyenne_ponderee = (P * E')/10; % moyenne pondérée des propositions
