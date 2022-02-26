2b4c9d810603debe98b80e038adcaa3781e2491a
jadeprevot <81019162+jadeprevot@users.noreply.github.com>
2022-02-26 21:47:00 +0100

    Update moyenne_ponderee.m
    Co-Authored-By: Aicha101 <92013849+Aicha101@users.noreply.github.com>

--- analyse_multicritere/moyenne_ponderee.m
+++ analyse_multicritere/moyenne_ponderee.m
@@ -1,3 +1,3 @@
 E = [6 5 4 5; 5 2 6 7; 4 3 2 5; 3 7 5 4; 1 7 2 9; 2 5 3 3; 5 4 2 9; 3 5 7 4]; %matrice jugements
-P = [1.5 4 3.5 1]; % matrice des poids
+P = [1 3.5 1.5 4]; % matrice des poids
 moyenne_ponderee = (P * E')/10; % moyenne pondérée des propositions
