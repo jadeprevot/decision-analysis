041db55196a150e66d32d84128fcdac5eeb119d4
Meryem <73638032+malamikasr@users.noreply.github.com>
2022-02-26 20:48:02 +0100

    Update ratios_ponderes.m

--- analyse_multicritere/ratios_ponderes.m
+++ analyse_multicritere/ratios_ponderes.m
@@ -1,5 +1,5 @@
 E = [6 5 4 5; 5 2 6 7; 4 3 2 5; 3 7 5 4; 1 7 2 9; 2 5 3 3; 5 4 2 9; 3 5 7 4]; %matrice jugements
-P = [1 4 3.5 1.5]; % matrice poids
+P = [1 3.5 1.5 4]; % matrice poids
 
 
 %Ratios par rapport à a
